7b6644e44fa8c572bf5c31fc1d9d30649801f8af
Mehin-Rahimli <mehincr-bp217@code.edu.az>
2024-11-01 05:13:17 -0700

    Initial commit

---
+++ JsonPractice/Customer.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonPractice
+{
+    internal class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber {  get; set; }
+        public static string PathString = Path.GetFullPath(Path.Combine("..", "..", "..", "Files", "Data", "customers.json"));
+        public Customer(int id, string firstname, string lastname, string phonenumber)
+        {
+            Id = id;
+            FirstName = firstname;
+            LastName = lastname;
+            PhoneNumber = phonenumber;
+        }
+        private List<Customer> GetJson(string path)
+        {
+            string newresult;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                newresult = sr.ReadToEnd();
+            }
+            var customers = JsonConvert.DeserializeObject<List<Customer>>(newresult);
+            if (customers == null)
+            {
+                customers = new List<Customer>();
+            }
+            return customers;
+
+            }
+        void SetJson(string path, List<Customer> customers)
+        {
+            string result = JsonConvert.SerializeObject(customers);
+            using StreamWriter sw = new(path);
+            sw.WriteLine(result);
+        }
+        public void Add(Customer customer)
+        {
+            var customers = GetJson(PathString);
+            customers.Add(customer);
+            SetJson(PathString, customers);
+        }
+        public Customer Search(int id)
+        {
+            var customers = GetJson(PathString);
+            return customers.FirstOrDefault(c => c.Id == id);
+        }
+
+        public void Delete(int id)
+        {
+            var customers = GetJson(PathString);
+            var customer = customers.FirstOrDefault(c => c.Id == id);
+
+            if (customer != null)
+            {
+                customers.Remove(customer);
+                SetJson(PathString, customers);
+                Console.WriteLine($"\n{customer.FirstName} silindi");
+            }
+            else
+            {
+                Console.WriteLine("Tapilmadi");
+            }
+        }
+
+    }
+}
---
+++ JsonPractice/Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace JsonPractice
+{
+    internal class Program
+    {
+        public static string PathString;
+
+        List<Customer> customers = new List<Customer>();
+          static void Main(string[] args)
+        {
+            
+            Directory.CreateDirectory(@"C:\Users\User\OneDrive\Desktop\Documents\SecondProject\JsonPractice\JsonPractice\Data");
+            File.Create(@"C:\Users\User\OneDrive\Desktop\Documents\SecondProject\JsonPractice\JsonPractice\Data\customers.json");
+
+            Customer customer = new Customer(1, "Sam", "Gilbert", "666");
+            Customer.Add(customer, customer);
+        }
+        
+
+    }
+}
